ca666e18338736c36a3edae9bb134a1bd2d95aec
William-Boissonneault-GPH <151781837+William-Boissonneault-GPH@users.noreply.github.com>
2025-03-22 23:46:46 -0400

    pertub

--- Developpement/SimulateurPhysique/identificateur_v3.py
+++ Developpement/SimulateurPhysique/identificateur_v3.py
@@ -17,10 +17,10 @@ AireTEC = 0.0156**2
 rho = 2700
 cp = 900
 
-T_amb = 24
-startTime = 0
+T_amb = 23
+startTime = 29
 endTime = 1000
-totalTime = 2000
+totalTime = 600
 courrantDEchelon = -3
 
 indiceFichier = 4
@@ -66,6 +66,33 @@ for nom in NomDeFichier:
         "T2" : T2,
         "T3" : T3
     })
+
+EssaiPerturb = []
+time=[]
+T1=[]
+T2=[]
+T3=[]
+nomfichier=r'C:\Users\willi\OneDrive - Université Laval\Cours\HIV2025\ModelisationGPH\LaseStab_modGPH\Developpement\SimulateurPhysique\Perturbation_temp_piece.csv'
+with open(nomfichier, mode="r", encoding="ISO-8859-1") as file:
+    reader = csv.reader(file)
+    next(reader)  # Skip the first header row
+    next(reader)  # Skip the duplicate header row
+
+    for row in reader:
+        time.append(int(row[0]))  # Convert time to integer
+        T1.append(float(row[1]))  # Convert temperatures to float
+        T2.append(float(row[2]))
+        T3.append(float(row[3]))
+T1 = np.array(T1)
+T2 = np.array(T2)
+T3 = np.array(T3)
+
+EssaiPerturb.append({
+    "time" : time,
+    "T1" : T1,
+    "T2" : T2,
+    "T3" : T3
+})
 
 
 dT_dt_fermetureTEC = []
@@ -274,18 +301,131 @@ def simulationMultiplesPuissanceTEC(courrantIndice, fileName, currentsPassed):
     #plt.legend()
     #plt.show()
 
+def simulationMultiplesPuissancePerturbateur():
+    
+    ## P = VI = V**2 / R = 5v**2 /25 = 1W
+    #Pour plusieurs simulation
+    PuissanceATester = np.linspace(0,1,7)
 
-Currents = [
-    [0.6, 0.8],
-    [0.6, 0.4],
-    [0.9,1.2],
-    [0.9,0.6],
-    [-0.6, -0.8],
-    [-0.6, -0.4],
-    [-0.9,-1.2],
-    [-0.9,-0.6]
-]
+    color = ['red','black','orange','blue','green','gray','purple']
 
-for cur in Currents:
-    filename = f"model_14fev_{str(cur[0]*1000)}_{str(cur[1]*1000)}.csv"
-    simulationMultiplesPuissanceTEC(indiceFichier, filename, cur)
+    Plaques = []
+    thermos = []
+    Tecs = []
+
+    position1 = (0.11875 - 0.014, 0.031)  
+    position2 = (0.11875 - 0.0604, 0.031) 
+    position3 = (0.11875 - 0.1065, 0.031) 
+        # Créer des instances de thermoresistance
+        
+    for PuissanceTec in PuissanceATester:
+        #plaque = PlaqueThermique((0.11875,0.062,0.002), (k_rhocp * (1/(cTec_rhocp / cTec)), 1, 1/(cTec_rhocp / cTec)), h_rhocp * (1/(cTec_rhocp / cTec)), (0.001,0.001), T_amb)
+        plaque = PlaqueThermique((0.11875,0.062,0.0016), (180, 900, 2700), 14, (0.001,0.001), T_amb)
+        Tec = ActionneurThermiqueSIMPLE((0.096, 0.031), (0.015,0.0156), plaque.matTemperature, plaque.dimensionsElementFinie)
+
+        thermo1 = thermo(position=position1, diamètre=0.008, épaisseur=0.001, plaque=plaque)
+        thermo2 = thermo(position=position2, diamètre=0.008, épaisseur=0.001, plaque=plaque)
+        thermo3 = thermo(position=position3, diamètre=0.008, épaisseur=0.001, plaque=plaque)
+
+        Plaques.append(plaque)
+        thermos.append([thermo1, thermo2, thermo3])
+        Tecs.append(Tec)
+
+    #garder le même ratio
+    #= 800
+    num_frames = int(1.5 * totalTime * 1000)
+    dTime = totalTime/num_frames
+    ##Nombre de frame skippé dans l'animation
+    animationStep = 1600
+
+    echelonCourant = 0
+
+    temperatures = [[],[],[],[],[],[],[]]
+    time = []
+
+    mat_perturbs = []
+
+    ###Genere perturbations
+    for j, plaque in enumerate(Plaques):
+        sourceChaleur = [{
+                "x": 0.08155,
+                "y": 0.031,
+                "puissance": PuissanceATester[j]
+            }]
+        mat_perturb = plaque.generer_mat_pertub(sourceChaleur)
+        mat_perturbs.append(mat_perturb)
+    
+    for j, tec in enumerate(Tecs):
+                    #tec.updateMatPerturbation(echelonCourant * PuissanceATester[j])
+                    tec.updateMatQTECCourrant(0)
+    
+    for i in range(num_frames):
+            ###Effectue un échelon d'opération à mi chemin
+            if i*dTime >= startTime:
+                for j, plaque in enumerate(Plaques):
+                    plaque.propagationDunPasDeTemps(dTime, T_amb, [Tecs[j].matQTEC, mat_perturbs[j]])
+            else:
+                for j, plaque in enumerate(Plaques):
+                    plaque.propagationDunPasDeTemps(dTime, T_amb, [Tecs[j].matQTEC])
+    
+            if i % animationStep == 0:
+                print(i)
+                for j, tec in enumerate(Tecs):
+                    #tec.updateMatPerturbation(echelonCourant * PuissanceATester[j])
+                    tec.updateMatQTECCourrant(0)
+
+                for j, thermistances in enumerate(thermos):
+                    temperatures[j].append([thermistances[0].lire_temperature(), thermistances[1].lire_temperature(), thermistances[2].lire_temperature()])
+                time.append(i*dTime)            
+
+    plt.plot(EssaiPerturb[0]["time"][0:], EssaiPerturb[0]["T3"][0:])
+    plt.plot(EssaiPerturb[0]["time"][0:], EssaiPerturb[0]["T2"][0:])
+    plt.plot(EssaiPerturb[0]["time"][0:], EssaiPerturb[0]["T1"][0:])
+    #tempAAfficher = []
+
+    for i, temps in enumerate(temperatures):
+        temp1AAfficher = []
+        temp2AAfficher = []
+        temp3AAfficher = []
+        for temp in temps:
+            temp1AAfficher.append(temp[0])
+            temp2AAfficher.append(temp[1])
+            temp3AAfficher.append(temp[2])
+        plt.plot(time, temp1AAfficher, label=f'{PuissanceATester[i]}', color=color[i])
+        plt.plot(time, temp2AAfficher, color=color[i])
+        plt.plot(time, temp3AAfficher, color=color[i])
+
+        
+
+    rows = zip(time, temp1AAfficher, temp2AAfficher, temp3AAfficher)
+
+    plt.show()
+
+    # Write to CSV
+    with open('test.csv', "w", newline="") as file:
+        writer = csv.writer(file)
+        # Write header
+        writer.writerow(["time(s)", "tempTec", "tempMilieu", "tempLaser"])
+        # Write data
+        writer.writerows(rows)
+
+    print("CSV file saved successfully!")
+
+
+
+simulationMultiplesPuissancePerturbateur()
+
+#Currents = [
+#    [0.6, 0.8],
+#    [0.6, 0.4],
+#    [0.9,1.2],
+#    [0.9,0.6],
+#    [-0.6, -0.8],
+#    [-0.6, -0.4],
+#    [-0.9,-1.2],
+#    [-0.9,-0.6]
+#]
+
+#for cur in Currents:
+#    filename = f"model_14fev_{str(cur[0]*1000)}_{str(cur[1]*1000)}.csv"
+#    simulationMultiplesPuissanceTEC(indiceFichier, filename, cur)
